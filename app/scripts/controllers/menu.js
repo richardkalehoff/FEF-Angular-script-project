c4eab16cb60768e9ecf7409f78f88f4e14ceac5c
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-05 14:58:47 -0500

    add content for ng-repeat

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,9 +9,25 @@
  */
 angular.module('udaciMealsApp')
   .controller('MenuCtrl', function () {
-    this.id = 'chipotle-shrimp-wrap';
-    this.name = 'Chipotle Shrimp Wrap';
-    this.img = 'chipotle-shrimp-wrap.jpg';
-    this.rating = 4.3;
-    this.url = 'https://www.eatclub.com/s/dish/chipotle-shrimp-vurvur/';
+    this.items = [
+      {
+        id: 'chipotle-shrimp-wrap',
+        name: 'Chipotle Shrimp Wrap',
+        img: 'chipotle-shrimp-wrap.jpg',
+        rating: 4.3,
+        url: 'https://www.eatclub.com/s/dish/chipotle-shrimp-vurvur/',
+      }, {
+        id: 'chicken-shawarma-rice-bowl',
+        name: 'Chicken Shawarma Rice Bowl',
+        img: 'chicken-shawarma-rice-bowl.jpg',
+        rating: 3.9,
+        url: 'https://www.eatclub.com/s/dish/Chicken-shawarma-rice-bowl-sajj/',
+      }, {
+        id: 'outrageous-chocolate-chip-cookies',
+        name: 'Outrageous Chocolate Chip Cookies',
+        img: 'outrageous-chocolate-chip-cookies.jpg',
+        rating: 4.8,
+        url: 'http://allrecipes.com/recipe/10141/outrageous-chocolate-chip-cookies/',
+      }
+    ];
   });
