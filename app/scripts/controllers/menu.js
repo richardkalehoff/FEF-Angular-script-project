0a2f0e60bd39b03b11922d2fde0bc59e9a5b4404
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-05 13:35:08 -0500

    add menu controller

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('MenuCtrl', function () {
+    this.id = 'chipotle-shrimp-wrap';
+    this.name = 'Chipotle Shrimp Wrap';
+    this.img = 'chipotle-shrimp-wrap.jpg';
+    this.rating = 4.2;
+    this.url = 'https://www.eatclub.com/s/dish/chipotle-shrimp-vurvur/';
+  });
