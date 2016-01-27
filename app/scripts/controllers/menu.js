86872e4ff5414b9dcf3e8338de2ec4123577f0a6
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-27 10:50:34 -0800

    add content for ng-repeat

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,8 +9,27 @@
  */
 angular.module('udaciMealsApp')
   .controller('MenuCtrl', function () {
-    this.id = 'strawberry-pudding';
-    this.name = 'Strawberry Pudding';
-    this.img = 'strawberry-pudding.jpg';
-    this.rating = 4.3;
+    this.items = [
+      {
+        id: 'chicken-pomegranate-salad',
+        name: 'Chicken Pomegranate Salad',
+        img: 'chicken-pomegranate-salad.jpg',
+        calories: 430,
+        rating: 4.1
+      },
+      {
+        id: 'strawberry-pudding',
+        name: 'Strawberry Pudding',
+        img: 'strawberry-pudding.jpg',
+        calories: 280,
+        rating: 5
+      },
+      {
+        id: 'ham-goat-cheese-croissant',
+        name: 'Ham and Goat Cheese Croissant',
+        img: 'ham-goat-cheese-croissant.jpg',
+        calories: 670,
+        rating: 3.9
+      }
+    ];
   });
