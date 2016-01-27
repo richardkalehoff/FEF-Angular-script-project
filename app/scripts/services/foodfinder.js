0a769b4e700d72c81bbd063eb074041e495b3b3c
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-27 11:27:56 -0800

    add foodFinder service

---
+++ app/scripts/services/foodfinder.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name udaciMealsApp.foodFinder
+ * @description
+ * # foodFinder
+ * Service in the udaciMealsApp.
+ */
+angular.module('udaciMealsApp')
+  .service('foodFinder', function () {
+    
+  });
