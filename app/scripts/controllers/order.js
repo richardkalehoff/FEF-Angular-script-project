b5af6440555fdaa00e6cc919ad4dfb0c3a8cc0ea
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-28 14:34:32 -0800

    create order Template/Controller

---
+++ app/scripts/controllers/order.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:OrderCtrl
+ * @description
+ * # OrderCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('OrderCtrl', function () {
+    
+  });
